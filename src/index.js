acd067c24cc4cd6ddd815fb7dd4aa6a341f9ca46
chall298 <chall361@gmail.com>
2021-01-19 16:04:11 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard> 
+                <div>
+                    <h4>WARNING!</h4>
+                    Are you sure?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="hey" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:00AM"  content="nice" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM"  content="cool" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />,document.querySelector('#root'))
